df1f1256f2ff775ae53ef38d165e579da13a290f
zwoneee <trungtien.2808gl@gmail.com>
2025-05-22 14:48:51 +0700

    [add] lab_02 ex01 caesar

--- lab_02/ex01/api.py
+++ lab_02/ex01/api.py
@@ -1,17 +1,9 @@
 from flask import Flask, request, jsonify
 from cipher.caesar import CaesarCipher
-from cipher.vigenere import VigenereCipher
-from cipher.railfence import RailFenceCipher
-from cipher.playfair import PlayfairCipher
-from cipher.transposition import TranspositionCipher
-
 app = Flask(__name__)
 
+#CAESAR CIPHER ALGORITHM
 caesar_cipher = CaesarCipher()
-vigenere_cipher = VigenereCipher()
-railfence_cipher = RailFenceCipher()
-playfair_cipher = PlayfairCipher()
-transposition_cipher = TranspositionCipher()
 
 
 @app.route("/api/caesar/encrypt", methods=["POST"])
@@ -25,92 +17,11 @@ def caesar_encrypt():
 @app.route("/api/caesar/decrypt", methods=["POST"])
 def caesar_decrypt():
     data = request.json
-    encrypted_text = data['cipher_text']
+    cipher_text = data['cipher_text']
     key = int(data['key'])
-    decrypted_text = caesar_cipher.decrypt_text(encrypted_text, key)
+    decrypted_text = caesar_cipher.decrypt_text(cipher_text, key)
     return jsonify({'decrypted_message': decrypted_text})
 
-@app.route('/api/vigenere/encrypt', methods=['POST'])
-def vigenere_encrypt():
-    data = request.json
-    plain_text = data['plain_text']
-    key = data['key']
-    
-    encrypted_text = vigenere_cipher.vigenere_encrypt(plain_text, key)
-    
-    return jsonify({'encrypted_text': encrypted_text})
-
-@app.route('/api/vigenere/decrypt', methods=['POST'])
-def vigenere_decrypt():
-    data = request.json
-    cipher_text = data['cipher_text']
-    key = data['key']
-    
-    decrypted_text = vigenere_cipher.vigenere_decrypt(cipher_text, key)
-    
-    return jsonify({'decrypted_text': decrypted_text})
-
-@app.route('/api/railfence/encrypt', methods=['POST'])
-def encrypt():
-    data = request.json
-    plain_text = data['plain_text']
-    key = int(data['key'])
-
-    encrypted_text = railfence_cipher.rail_fence_encrypt(plain_text, key)
-    
-    return jsonify({'encrypted_text': encrypted_text})
-
-@app.route('/api/railfence/decrypt', methods=['POST'])
-def decrypt():
-    data = request.json
-    cipher_text = data['cipher_text']
-    key = int(data['key'])
-
-    decrypted_text = railfence_cipher.rail_fence_decrypt(cipher_text, key)
-
-    return jsonify({'decrypted_text': decrypted_text})
-
-@app.route('/api/playfair/creatematrix', methods=['POST'])
-def playfair_creatematrix():
-    data = request.json
-    key = data['key']
-    playfair_matrix = playfair_cipher.create_playfair_matrix(key)
-    return jsonify({"playfair_matrix": playfair_matrix})
-
-@app.route('/api/playfair/encrypt', methods=['POST'])
-def playfair_encrypt():
-    data = request.json
-    plain_text = data['plain_text']
-    key = data['key']
-    playfair_matrix = playfair_cipher.create_playfair_matrix(key)
-    encrypted_text = playfair_cipher.playfair_encrypt(plain_text, playfair_matrix)
-    return jsonify({'encrypted_text': encrypted_text})
-
-@app.route('/api/playfair/decrypt', methods=['POST'])
-def playfair_decrypt():
-    data = request.json
-    cipher_text = data['cipher_text']
-    key = data['key']
-    playfair_matrix = playfair_cipher.create_playfair_matrix(key)
-    decrypted_text = playfair_cipher.playfair_decrypt(cipher_text, playfair_matrix)
-    return jsonify({'decrypted_text': decrypted_text})
-
-@app.route('/api/transposition/encrypt', methods=['POST'])
-def transposition_encrypt():
-    data = request.get_json()
-    plain_text = data.get('plain_text')
-    key = int(data.get('key'))
-    encrypted_text = transposition_cipher.encrypt(plain_text, key)
-    return jsonify({'encrypted_text': encrypted_text})
-
-@app.route('/api/transposition/decrypt', methods=['POST'])
-def transposition_decrypt():
-    data = request.get_json()
-    cipher_text = data.get('cipher_text')
-    key = int(data.get('key'))
-    decrypted_text = transposition_cipher.decrypt(cipher_text, key)
-    return jsonify({'decrypted_text': decrypted_text})
-
-if __name__ == '__main__':
-    app.run(debug=True)
-    
+#main function
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
